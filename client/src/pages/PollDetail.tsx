c936c51b8c7153c8a92fd90a576b5dbb1edbc2d4
Kamal ELMAGOURI <kamalELMAGOURI@gmail.com>
2026-01-11 17:10:35 +0100

    Remove unused useEffect import

--- client/src/pages/PollDetail.tsx
+++ client/src/pages/PollDetail.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { useQuery, useMutation } from '@apollo/client'
 import { GET_POLL, VOTE } from '../queries'
